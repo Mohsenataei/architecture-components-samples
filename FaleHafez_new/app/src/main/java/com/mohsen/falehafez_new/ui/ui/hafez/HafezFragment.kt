f49da174a66333636ddc99296b85cba7e104ebef
mohsen <mohsenataeiasad@gmail.com>
2019-11-09 16:40:54 +0330

    navigation drawer items changed.

---
+++ FaleHafez_new/app/src/main/java/com/mohsen/falehafez_new/ui/ui/hafez/HafezFragment.kt
@@ -0,0 +1,26 @@
+package com.mohsen.falehafez_new.ui.ui.hafez
+
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+import com.mohsen.falehafez_new.R
+
+/**
+ * A simple [Fragment] subclass.
+ */
+class HafezFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_hafez, container, false)
+    }
+
+
+}
---
+++ FaleHafez_new/app/src/main/java/com/mohsen/falehafez_new/ui/ui/home/HomeViewModel.kt
@@ -0,0 +1,13 @@
+package com.mohsen.falehafez_new.ui.ui.home
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class HomeViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is home Fragment"
+    }
+    val text: LiveData<String> = _text
+}
